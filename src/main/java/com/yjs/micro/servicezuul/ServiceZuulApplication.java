00abbcef40399bb81eaa7e8546d3b3dc1679c7b6
yangshuyi <yangshuyi@yunjie-system.com>
2018-03-29 16:17:54 +0800

    网关的初步配置

---
+++ src/main/java/com/yjs/micro/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,16 @@
+package com.yjs.micro.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy //开启网关
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+}
